f9affe5a2837f1ffef4126bf703f3389f165cb17
Av Pinzur <av@avpinzur.com>
2016-09-29 06:24:48 -0500

    Scaffolded solution.

---
+++ Core.Test/GameTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AvP.TicTacToe.Core.Test
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
